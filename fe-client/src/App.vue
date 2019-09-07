312ac0bad5b908ccdd72d41fef85c4cb418a7743
Josh Sanderson <josh@jgstechnical.com>
2019-09-07 08:38:19 -0700

    Fix vuetify eslint issues and start initial view organization

--- fe-client/src/App.vue
+++ fe-client/src/App.vue
@@ -1,36 +1,41 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-toolbar-title>Morgan's Player</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container
+        class=""
+        fluid
+      >
+        <v-row
+          align="left"
+          justify="top"
+        >
+          <v-col class="shrink">
+            <router-view/>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
+
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
-};
+  components: {},
+  data: () => ({}),
+  created () {
+    this.$vuetify.theme.dark = true
+  }
+}
 </script>
