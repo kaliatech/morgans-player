cf516bd60c12060037be7157cded9fd85b237db1
Josh Sanderson <josh@jgstechnical.com>
2019-09-09 14:11:50 -0700

    Impl another 10%, including basic video handling

--- fe-client/src/views/PlaylistView.vue
+++ fe-client/src/views/PlaylistView.vue
@@ -2,30 +2,42 @@
   <v-container>
     <v-row>
       <v-col>
-        <h1>
+        <h3>
           {{ playlistName }} <span v-if="selectedS3Obj"> - {{ selectedS3Obj.key }}</span>
-        </h1>
+        </h3>
       </v-col>
     </v-row>
 
     <v-row>
-      <v-col>
-        <audio ref="audioPlayer" class="media-player" controls @pause="onMediaPause" @ended="onMediaEnded"
-               @play="onMediaPlay" :style="selectedS3Obj ? '' : 'visibility:hidden'">
+      <v-col v-if="playlistHasVideos === false">
+        <audio ref="audioPlayer" class="media-player audio-player playlist-audio-only" controls @pause="onMediaPause"
+               @ended="onMediaEnded"
+               @play="onMediaPlay" :style="selectedS3Obj && !isVideoFile(selectedS3Obj.key) ? '' : 'display:none'">
           <source v-if="selectedS3Obj" :src="selectedS3Obj.signedUrl" type="audio/mpeg"/>
         </audio>
-        <!--        <video :src=""></video>-->
+      </v-col>
+      <v-col v-if="playlistHasVideos === true">
+        <audio ref="audioPlayer" class="media-player audio-player playlist-has-videos" controls @pause="onMediaPause"
+               @ended="onMediaEnded"
+               @play="onMediaPlay" :style="selectedS3Obj && !isVideoFile(selectedS3Obj.key) ? '' : 'display:none'">
+          <source v-if="selectedS3Obj" :src="selectedS3Obj.signedUrl" type="audio/mpeg"/>
+        </audio>
+        <video ref="videoPlayer" class="media-player video-player playlist-has-videos" controls @pause="onMediaPause"
+               @ended="onMediaEnded"
+               @play="onMediaPlay" :style="selectedS3Obj && isVideoFile(selectedS3Obj.key) ? '' : 'display:none'">
+          <source v-if="selectedS3Obj" :src="selectedS3Obj.signedUrl" :type="getMimeType(selectedS3Obj.key)"/>
+        </video>
       </v-col>
     </v-row>
     <v-row>
       <v-col>
 
         <v-card
-          v-if="data.keys"
+          v-if="data.s3Objs"
           class="mx-auto"
           tile>
 
-          <v-list-item v-for="s3Obj in data.keys" :key="s3Obj.key" two-line @click="onPlayPauseClick(s3Obj)"
+          <v-list-item v-for="s3Obj in data.s3Objs" :key="s3Obj.key" two-line @click="onPlayPauseClick(s3Obj)"
                        :class="selectedS3Obj === s3Obj ? 'v-list-item--active' : ''">
             <v-list-item-avatar>
               <v-icon v-if="selectedS3Obj === s3Obj && mediaState === MEDIA_STATE.PLAYING" dark large>mdi-pause
@@ -34,9 +46,9 @@
             </v-list-item-avatar>
             <v-list-item-content>
               <v-list-item-title>
-                {{ s3Obj.key }}
+                {{ s3Obj.title }}
               </v-list-item-title>
-              <v-list-item-subtitle>{{ s3Obj.size }}</v-list-item-subtitle>
+              <v-list-item-subtitle>{{ formatFilesize(s3Obj.size) }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
         </v-card>
@@ -47,6 +59,8 @@
 
 <script>
 import axios from 'axios'
+
+import filesize from 'filesize'
 
 const MEDIA_STATE = {
   'PLAYING': 'playing',
@@ -62,7 +76,16 @@ export default {
       data: {},
       selectedS3Obj: null,
       mediaState: MEDIA_STATE.STOPPED,
-      MEDIA_STATE: MEDIA_STATE
+      MEDIA_STATE: MEDIA_STATE,
+      mediaPlayer: null
+    }
+  },
+  computed: {
+    playlistHasVideos () {
+      if (!this.data || !this.data.s3Objs) {
+        return false
+      }
+      return this.data.s3Objs.some((it) => this.isVideoFile(it.key))
     }
   },
   created () {
@@ -75,10 +98,14 @@ export default {
         accept: 'application/json'
       }
     }
+    // TODO: Make this env specific
     axios.get('https://1wjtfz73pe.execute-api.us-west-1.amazonaws.com/beta/playlist/' + this.playlistName, config)
       .then((resp) => {
         let respData = resp.data
-        respData.keys = respData.keys.filter((s3Obj) => !s3Obj.key.endsWith('playlist.json'))
+        respData.s3Objs = respData.s3Objs.filter((s3Obj) => !s3Obj.key.endsWith('playlist.json'))
+        respData.s3Objs.forEach((s3Obj) => {
+          s3Obj.title = s3Obj.key.replace(respData.path + '/', '')
+        })
         this.data = respData
         if (!this.data.playlist.title) {
           this.data.playlist.title = this.playlistName
@@ -100,11 +127,17 @@ export default {
   watch: {
     selectedS3Obj: function (newVal, oldVal) {
       this.$nextTick(() => {
-        if (this.$refs && this.$refs.audioPlayer) {
-          this.$refs.audioPlayer.load()
-          this.$refs.audioPlayer.play()
-          this.mediaState = MEDIA_STATE.PLAYING
+        if (!this.$refs) {
+          return
         }
+        if (this.isVideoFile(this.selectedS3Obj.key)) {
+          this.mediaPlayer = this.$refs.videoPlayer
+        } else {
+          this.mediaPlayer = this.$refs.audioPlayer
+        }
+        this.mediaPlayer.load()
+        this.mediaPlayer.play()
+        this.mediaState = MEDIA_STATE.PLAYING
       })
     }
   },
@@ -112,9 +145,9 @@ export default {
     onPlayPauseClick (s3Obj) {
       if (s3Obj === this.selectedS3Obj && this.mediaState === MEDIA_STATE.PLAYING) {
         this.mediaState = MEDIA_STATE.PAUSED
-        this.$refs.audioPlayer.pause()
+        this.mediaPlayer.pause()
       } else if (s3Obj === this.selectedS3Obj) {
-        this.$refs.audioPlayer.play()
+        this.mediaPlayer.play()
         this.mediaState = MEDIA_STATE.PLAYING
       } else {
         this.selectedS3Obj = s3Obj
@@ -128,9 +161,33 @@ export default {
     },
     onMediaEnded () {
       this.mediaState = MEDIA_STATE.STOPPED
-      if (this.data.keys.slice(-1) !== this.selectedS3Obj) {
-        this.selectedS3Obj = this.data.keys[this.data.keys.indexOf(this.selectedS3Obj) + 1]
+      if (this.data.s3Objs.slice(-1) !== this.selectedS3Obj) {
+        this.selectedS3Obj = this.data.s3Objs[this.data.s3Objs.indexOf(this.selectedS3Obj) + 1]
       }
+    },
+    isVideoFile (filename) {
+      return this.getMimeType(filename).startsWith('video')
+    },
+    getMimeType (filename) {
+      if (!filename) {
+        return ''
+      }
+      const ext = filename.split('.').pop()
+      switch (ext) {
+        case 'mov':
+          return 'video/quicktime'
+        case 'mp3':
+          return 'audio/mpeg'
+        case 'mp4':
+          return 'video/mp4'
+        case 'wav':
+          return 'audio/wav'
+        default:
+          return ''
+      }
+    },
+    formatFilesize (size) {
+      return filesize(size, {round: 1})
     }
   }
 }
@@ -142,6 +199,13 @@ export default {
 
   .media-player {
     width: 100%;
+  }
+
+  .playlist-audio-only {
+  }
+
+  .playlist-has-videos {
+    height: 20rem;
   }
 
   @-webkit-keyframes spin /* Safari and Chrome */
